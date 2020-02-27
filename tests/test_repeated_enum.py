1304330a4116d9bb05e5949cbf4129ba87a9422e
George Hilliard <ghilliard@kopismobile.com>
2020-02-27 16:26:20 -0600

    Do not reject enum values that are not listed in the proto file
    According to the language guide [1]:
    
        During deserialization, unrecognized enum values will be preserved
        in the message, though how this is represented when the message is
        deserialized is language-dependent.
    
    We can store any integer in the underlying field, because in Cython,
    enum and int are the same.
    
    Fixes appnexus/pyrobuf#149.

--- tests/test_repeated_enum.py
+++ tests/test_repeated_enum.py
@@ -17,12 +17,33 @@ class RepeatedEnumTest(unittest.TestCase):
         message1.list_enum.append(2)
         message1.list_enum.append(0)
         message1.list_enum.append(1)
+        # Must be able to handle arbitrary integers in enums:
+        message1.list_enum.append(42)
+        # ... but not arbitrary values:
+        with self.assertRaises(TypeError):
+            message1.list_enum.append('not an int')
 
         buf = message1.SerializeToString()
         message2.ParseFromString(buf)
 
         for i in range(len(message1.list_enum)):
             self.assertEqual(message1.list_enum[i], message2.list_enum[i])
+
+    def test_single_enum_serde(self):
+        message1 = TestRepeatedEnum()
+        message2 = TestRepeatedEnum()
+
+        message1.single_enum = 1
+        # Must be able to handle arbitrary integers in enums:
+        message1.single_enum = 42
+        # ... but not arbitrary values:
+        with self.assertRaises(TypeError):
+            message1.single_enum = 'not an int'
+
+        buf = message1.SerializeToString()
+        message2.ParseFromString(buf)
+
+        self.assertEqual(message1.single_enum, message2.single_enum)
 
 if __name__ == "__main__":
     unittest.main()
